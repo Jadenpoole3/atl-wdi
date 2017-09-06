ba3c6e2b054cc09f49c7f0537f40f5b3422a4616
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-09-05 21:56:37 -0400

    update data modeling hw

---
+++ homework/javaScript/js-data-modeling/hw.js
@@ -0,0 +1,72 @@
+let Trackit = {
+    time: 30,
+    task: 5,
+    projects: 5,
+
+}
+// This app will help tracking and managing tasks.
+
+let Instagram = {
+    picture_taking: 1,
+    video_taking: 1,
+    messaging: 25,
+}
+
+// These cases will help uploading pictures and videos.
+// They will also help you communicate with your friends
+
+let Hue = {
+    temperatue: 72,
+    lights: off,
+    door_lock: locked,
+}
+
+// This app will make your life much easier using the keys as 
+//well
+
+let Footlocker = {
+    add_cart: 1,
+    sneakers: 1000,
+    jordans: 100,
+
+}
+
+// This app will help you to have a faster checkout. It also 
+// has a wide range of different sneakers.
+
+// 5
+// The first one is much easier for people to recognize which 
+//station they will depart from.
+//  The Second one is much easier for people to recognize
+//which line or path they will be taking
+
+//6
+// The advantages of the first one is that its more helpful 
+//for patients who need phlebotomy. The second one is more helpful
+//for patients who need internal medicine
+
+// 7
+
+//a
+let X = {
+    placement: X,
+    letter: x,
+    name: X,
+}
+
+let O = {
+    placement: O,
+    letter: O,
+    name: O,
+}
+
+//b
+let O = {
+    placement: O,
+    letter: O,
+    name: O,
+
+// This how it would be it shown 
+
+//c 
+//These represent X and O when it comes to tic-tac-toe
