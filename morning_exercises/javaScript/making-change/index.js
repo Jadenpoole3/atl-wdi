d4eabc27eab4694f92f6da57fb5ad7863a680f80
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-09-08 11:45:24 -0400

    homework

--- morning_exercises/javaScript/making-change/index.js
+++ morning_exercises/javaScript/making-change/index.js
@@ -5,8 +5,54 @@
 /// DO NOT EDIT ABOVE THIS LINE ///
 
 var makeChange = function(price, payment) {
-  // Your Code Here
+  price *= 100;
+  payment *= 100;
+  // Write a function that can make correct change, given a purchase price and an amount paid.
+//denoinations
+//return an array like [Q,D,N,P]
+
+let Change = payment - price 
+
+ let changeLeft= Math.round(payment * 100 -price * 100);
+if(changeLeft <= 0) {return[0,0,0,0] 
+} else {
+
+  const quarters = Math.floor(change / 25);
+  change = change % 25
+
+
+  const dimes = Math.floor(change / 10);
+  change = change % 10
+
+
+
+  const nickels = Math.floor(change / 5);
+  change = change % 5
+
+
+
+  const pennies = change;
+
+
+}
+return [quarters,dimes,nickels, pennies];
+/* Here's an example of how this should work.
+Say we want to buy something for $5.60, and we pay $6.00;
+we should get back 1 quarter, 1 dime, and 1 nickel. If the item was, instead,
+$5.62, we would get back 1 quarter, 1 dime, and 3 pennies.
+
+```js
+makeChange(5.60, 6.00) // => [1, 1, 1, 0]
+makeChange(5.62, 6.00) // => [1, 1, 0, 3]
+//                            Q, D, N, P
+*/
+
+
+
+
 };
+return results
+make
 
 /// DO NOT EDIT BELOW THIS LINE ///
 module.exports = {
