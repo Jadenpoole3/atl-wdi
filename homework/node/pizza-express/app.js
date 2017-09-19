2b33ccde09f6a9f2df4dfd7634e34cb779fc00ea
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-09-19 17:49:34 -0400

    completed GET routes

--- homework/node/pizza-express/app.js
+++ homework/node/pizza-express/app.js
@@ -1,15 +1,35 @@
 // app.js
-
-//require express package
-var express = require('express');
+const express = require('express');
 //save an express module as 'app'
-var app     = express();
-// assigning 3000 as our port
-var port    = 3000;
+const app = express();
+const hbs = require('hbs');
 
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+res.send('Welcome to Pizza Express');
+});
+
+app.get("/topping/:type", (req, res, next) => {
+    const type = req.params.type;
+    res.send(`${type} pizza! Good choice.`)
+ })
+
+ app.get('/order/:amount/:size', (req, res, next) => {
+    const amount = req.params.amount;
+    const size = req.params.size;
+         res.send(`${amount} ${size} Your order for pizza will be ready in 1 minute ` );
+     });
+
+
+
+
+
+const PORT = 3000;
 // tells the server to listen for requests on port 3000
-app.listen(port, function(){
+app.listen(PORT, function(){
   console.log("==========================")
-  console.log('LISTENING ON PORT ' + port);
+  console.log('LISTENING ON PORT ' + PORT);
   console.log("==========================")
 });
